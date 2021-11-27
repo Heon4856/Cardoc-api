66ff6738e193a4258e04d8d6ef5d975ad66b4a08
Sungeheon <elon4856@gmail.com>
2021-11-27 17:19:12 +0900

    refactor: url 및 함수 이름 변경.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
+@app.post("/auth/register", response_model=schemas.User)
 def create_user(user: schemas.UserRegister, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_register_id(db, register_id=user.register_id)
     if db_user:
@@ -29,12 +29,13 @@ def create_user(user: schemas.UserRegister, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/{user_id}", response_model=schemas.UserRegister)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+@app.get("/auth/login", response_model=schemas.UserRegister)
+def login(user: schemas.UserRegister, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_register_id(db, register_id=user.register_id)
 
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
+
 
     return db_user
 
