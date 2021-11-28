ee960e3f8fcc43b57f5a3a1f875ae95585b408dd
Sungeheon <elon4856@gmail.com>
2021-11-28 15:11:10 +0900

    add: tire router 등록 및 관련 schema등록.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI
 
 from sql_app import models
 from sql_app.database import engine
-from api import user
+from api import user, tire
 
 models.Base.metadata.create_all(bind=engine)
 
 def create_app():
     app = FastAPI(title="Cardoc Restfull API",)
     app.include_router(user.router)
+    app.include_router(tire.router)
 
     return app
 
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel
+from typing import List
+
 
 class UserRegister(BaseModel):
     register_id: str = None
     pw: str = None
+
+
+class TireRegister(BaseModel):
+    id: str = None
+    trimId: int = None
+
+
+class TireRegisterList(BaseModel):
+    tire_register_list: List[TireRegister] = None
 
 
 class Token(BaseModel):
@@ -10,10 +21,10 @@ class Token(BaseModel):
 
 
 class User(BaseModel):
-    id :int
-    register_id :str
-    hashed_password : str
-    is_active : bool
+    id: int
+    register_id: str
+    hashed_password: str
+    is_active: bool
 
     class Config:
         orm_mode = True
