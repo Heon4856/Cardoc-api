c38b80dde37d19901de3d8cc7a196fc8eeca8ead
Sungeheon <elon4856@gmail.com>
2021-11-29 12:15:31 +0900

    refactor: 이름을 좀더 명확하게 변경하였음.

--- service/service_tire.py
+++ service/service_tire.py
@@ -17,28 +17,28 @@ def save_tire_info(car_info, db, tire_register, trim_id, user_id):
     rear_tire = car_info["spec"]["driving"]["rearTire"]["value"]
 
     if is_tire_format(front_tire):
-        width, flat, wheel = re.split('[/R]', front_tire)
+        width, flatness_ratio, wheel_size = re.split('[/R]', front_tire)
         create_tire_info(db,
                          models.Tire(
                              user_id=user_id,
                              trim_id=trim_id,
                              position=TirePosition.FRONT,
                              width=width,
-                             flatness_ratio=flat,
-                             wheel_size=wheel
+                             flatness_ratio=flatness_ratio,
+                             wheel_size=wheel_size
                          )
                          )
         success_list.append(f'{tire_register} front_tire')
 
     if is_tire_format(rear_tire):
-        width, flat, wheel = re.split('[/R]', rear_tire)
+        width, flatness_ratio, wheel_size = re.split('[/R]', rear_tire)
         create_tire_info(db, models.Tire(
             user_id=user_id,
             trim_id=trim_id,
             position=TirePosition.REAR,
             width=width,
-            flatness_ratio=flat,
-            wheel_size=wheel
+            flatness_ratio=flatness_ratio,
+            wheel_size=wheel_size
         )
         )
         success_list.append(f'{tire_register} rear_tire')
